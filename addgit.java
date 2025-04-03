3b8ea621af3fd8306205a979e7abde2eeef35d01
vedika <vedik@vedika.com>
2025-04-03 15:21:13 +0530

    java pgm

---
+++ addgit.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.lang.*;
+public Static void main(String[]args){
+Scanner Sc = new Scanner(System.in);
+System.out.println("Enter first no.");
+System.out.println("Enter second no.");
+int a= Sc.nextInt();
+int b= Sc.nextInt();
+int sum = a+b;
+System.out.println("Addition of two no:"+sum);
+sc.close();
+}}
+
+
+
+
+}
+
